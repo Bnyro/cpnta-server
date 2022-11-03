54a57124ffba58d08548d8661bc504aa1d9aa63e
Bnyro <bnyro@tutanota.com>
2022-11-03 19:54:10 +0100

    add dropdown menu

--- lib/widgets/note_widget.dart
+++ lib/widgets/note_widget.dart
@@ -19,6 +19,60 @@ class NoteWidget extends StatefulWidget {
 }
 
 class _NoteWidgetState extends State<NoteWidget> {
+  Offset _tapPosition = Offset.zero;
+  void _getTapPosition(TapDownDetails details) {
+    final RenderBox referenceBox = context.findRenderObject() as RenderBox;
+    setState(() {
+      _tapPosition = referenceBox.globalToLocal(details.globalPosition);
+    });
+  }
+
+  void _deleteNote() {
+    deleteNote(widget.note.id!);
+    widget.refreshNotes();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text('Note deleted!'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            createNote(widget.note.title, widget.note.content);
+            widget.refreshNotes();
+          },
+        ),
+      ),
+    );
+  }
+
+  void _showContextMenu(BuildContext context) async {
+    final RenderObject? overlay =
+        Overlay.of(context)?.context.findRenderObject();
+
+    final result = await showMenu(
+        context: context,
+
+        // Show the context menu at the tap location
+        position: RelativeRect.fromRect(
+            Rect.fromLTWH(_tapPosition.dx, _tapPosition.dy, 30, 30),
+            Rect.fromLTWH(0, 0, overlay!.paintBounds.size.width,
+                overlay.paintBounds.size.height)),
+
+        // set a list of choices for the context menu
+        items: [
+          const PopupMenuItem(
+            value: 'delete',
+            child: Text('Delete'),
+          )
+        ]);
+
+    // Implement the logic for each choice here
+    switch (result) {
+      case 'delete':
+        _deleteNote();
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -30,22 +84,8 @@ class _NoteWidgetState extends State<NoteWidget> {
                         refreshNotes: widget.refreshNotes,
                       )))
             },
-        onDoubleTap: () => {
-              deleteNote(widget.note.id!),
-              widget.refreshNotes(),
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: const Text('Note deleted!'),
-                  action: SnackBarAction(
-                    label: 'Undo',
-                    onPressed: () {
-                      createNote(widget.note.title, widget.note.content);
-                      widget.refreshNotes();
-                    },
-                  ),
-                ),
-              )
-            },
+        onLongPress: () => _showContextMenu(context),
+        onTapDown: (details) => _getTapPosition(details),
         child: Card(
             margin: const EdgeInsets.all(16.0),
             child: Center(
