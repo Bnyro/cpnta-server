77e2ed7fcf9a9b68f90e7314859bf67a51828bdb
Bnyro <bnyro@tutanota.com>
2022-11-19 11:25:23 +0100

    lookup the api URL to check for internet

--- lib/providers/network_provider.dart
+++ lib/providers/network_provider.dart
@@ -1,8 +1,13 @@
 import 'dart:io';
 
+import 'package:cpnta/constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 Future<bool> hasNetwork() async {
+  final prefs = await SharedPreferences.getInstance();
+  final apiUrl = await prefs.getString("apiUrl") ?? defaultApiUrl;
   try {
-    final result = await InternetAddress.lookup('example.com');
+    final result = await InternetAddress.lookup(apiUrl);
     return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
   } on SocketException catch (_) {
     return false;
